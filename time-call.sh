820a0e773ce06ecb358da19299227f4fe0d1582e
root <root@neo>
2021-04-16 17:33:53 +0300

    a

--- time-call.sh
+++ time-call.sh
@@ -3,7 +3,7 @@ rm sample.txt
 touch sample.txt
 end=$((SECONDS+5))
 while [ $SECONDS -lt $end ]; do
-	echo "$(date)" >> sample.txt && /bin/python3 http-rest.py >> sample.txt
+	echo "$(date)" >> sample.txt && python http-rest.py >> sample.txt
 done
 
 
