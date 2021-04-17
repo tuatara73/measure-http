4c18026b9a0bfa1c819fda7e2fcb9c3027006bab
shiran <shiran@mail.com>
2021-04-17 17:53:15 +0300

    a

--- http-rest.py
+++ http-rest.py
@@ -1,7 +1,4 @@
 import requests
-import time
-import schedule
-import sys
 
 def Response():
     url = 'https://www.google.com'
@@ -15,22 +12,6 @@ def Response():
 Response()
 
 
-
-
-
-#sys.stdout = open('sample.txt', 'a')
-#schedule.every(5).seconds.do(Response)
-#sys.stdout.close()
-
-
-
-
-#while True:
-#    schedule.run_pending()
-#    time.sleep(1)
-#    f = open('sample.txt', 'a')
-#    f.write(str(Response))
-#    f.close
 
 
 
